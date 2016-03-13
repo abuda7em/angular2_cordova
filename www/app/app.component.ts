8db4045887df0c06d71edcd86f9b8ce8d40c7f0c
Abu_Da7em <you@example.com>
2016-03-13 16:28:32 +0300

    Added Angular2

---
+++ www/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector:'my-app',
+  template:'Device Is Ready, Angular2 is working'
+})
+export class AppComponent{
+
+}
---
+++ www/js/index.js
@@ -0,0 +1,12 @@
+function onDeviceReady() {
+    System.config({
+        packages: {
+          app: {
+            format: 'register',
+            defaultExtension: 'js'
+          }
+        }
+      });
+      System.import('app/boot').then(null, console.error.bind(console));
+}
+document.addEventListener("deviceready", onDeviceReady, false);
